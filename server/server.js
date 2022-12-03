892fd55e051c28ffaa23d598b82667aff12dd205
William Chalk <3halk1@gmail.com>
2022-12-02 20:51:44 -0500

    added concurrently scripts for easy development access

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("../client/"));
+
+require("../server/routes/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+  console.log(`Now Listening on Port ${PORT}`);
+});

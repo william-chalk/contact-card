d854cc3ef45813f0399b2724ae6668f1fd1c47bf
William Chalk <3halk1@gmail.com>
2022-12-04 14:23:44 -0500

    added indexd db

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -6,8 +6,10 @@ import "./submit";
 import "../css/index.css";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { initdb } from "./database";
 
 window.addEventListener("load", function () {
+  initdb();
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;

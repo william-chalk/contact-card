c25c1c770b6e1f3ecb1d5c8f5e1f0ea61a9b61a5
William Chalk <3halk1@gmail.com>
2022-12-04 15:41:35 -0500

    completed CRUD with adding update method functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -69,3 +69,25 @@ export const deleteDb = async (id) => {
 
   return result?.value;
 };
+
+export const editDb = async (id, name, email, phone, profile) => {
+  console.log("PUT to the database", id);
+
+  const contactDb = await openDB("contact_db", 1);
+
+  const tx = contactDb.transaction("contacts", "readwrite");
+
+  const store = tx.objectStore("contacts");
+
+  const request = store.put({
+    id: id,
+    name: name,
+    email: email,
+    phone: phone,
+    profile: profile,
+  });
+
+  const result = await request;
+
+  console.log("data saved to the database!", result);
+};
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -5,7 +5,7 @@ import { toggleForm, clearForm } from "./form";
 import "../css/index.css";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { initdb, getDb, postDb, deleteDb } from "./database";
+import { initdb, getDb, postDb, deleteDb, editDb } from "./database";
 import { fetchCards } from "./cards";
 
 window.addEventListener("load", function () {
@@ -39,6 +39,12 @@ form.addEventListener("submit", (event) => {
   if (submitBtnToUpdate == false) {
     postDb(name, email, phone, profile);
   } else {
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let profile = document.querySelector('input[type="radio"]:checked').value;
+
+    editDb(profileId, name, email, phone, profile);
     fetchCards();
     // Toggles the submit button back to POST functionality
     submitBtnToUpdate = false;
@@ -56,4 +62,18 @@ window.deleteCard = (e) => {
   let id = parseInt(e.id);
   deleteDb(id);
   fetchCards();
+};
+
+window.editCard = (e) => {
+  let editName = e.dataset.name;
+  let editEmail = e.dataset.email;
+  let editPhone = e.dataset.phone;
+
+  document.getElementById("name").value = editName;
+  document.getElementById("email").value = editEmail;
+  document.getElementById("phone").value = editPhone;
+
+  form.style.display = "block";
+
+  submitBtnToUpdate = true;
 };

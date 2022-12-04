12f6abd57ee1947aab8f0bb0a40cdf9302189a48
William Chalk <3halk1@gmail.com>
2022-12-04 13:53:40 -0500

    added webpack dependencies

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("../client/"));
+app.use(express.static("../client/dist/"));
 
 require("../server/routes/htmlRoutes")(app);
 
